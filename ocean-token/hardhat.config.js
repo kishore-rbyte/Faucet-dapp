04fc0ef672c3f9753e7d6fe5ef338dccca74450d
Kishore N <kishor.a1411@gmail.com>
2022-12-14 13:40:13 +0530

    first commit

--- ocean-token/hardhat.config.js
+++ ocean-token/hardhat.config.js
@@ -8,11 +8,11 @@ module.exports = {
 
     goerli: {
       url:'https://eth-goerli.g.alchemy.com/v2/6eU35ojLK-SsaJFx0qI1RPa8wPmNgkGr',
-      accounts: ['d40984b4ee6ed8aefe9121bfbab1777f7439acc8f3d02fff43ed57e720ed0ea9']
+      accounts: ['']
     },
     mumbai: {
       url:'https://polygon-mumbai.g.alchemy.com/v2/Z2dhBl2k_S-2bamZm444JaXl3mmhGvprs',
-      accounts: ['d40984b4ee6ed8aefe9121bfbab1777f7439acc8f3d02fff43ed57e720ed0ea9']
+      accounts: ['']
     } 
 
   }
